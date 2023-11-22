149ef43f73e3c18d0b5f326b6747e6f0c8c7bca1
Kaisa Silkelä <kaisa.silkela@sofiadigital.com>
2023-11-22 16:15:03 +0200

    Extending tests for add, toString, divide, filter and isArrayLikeObject

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -13,6 +13,7 @@ import isArrayLikeObject from '../src/isArrayLikeObject.js';
 // divide
 // filter
 // isArrayLikeObject
+
 // ceil
 // difference
 // toInteger
@@ -35,9 +36,61 @@ describe('add', () => {
     test('returns the correct value when one of the arguments is zero', () => {
         expect(add(2, 0)).toBe(2);
     });
+    
+    test('returns the correct value when only one argument is passed', () => {
+        expect(add(2)).toBe(2);
+    });
+
+    test('returns the correct value when one of the arguments is undefined', () => {
+        expect(add(2, undefined)).toBe(2);
+    });
+
+    test('returns 0 when both arguments are undefined', () => {
+        expect(add(undefined, undefined)).toBe(0);
+    });
+
+    test('returns NaN when and throws error one of the arguments is not a number', () => {
+        expect(add(2, 'hello')).toBeNaN();
+        expect(add(2, 'hello')).toThrow(Error('Invalid argument "hello"'));
+    });
+
+    test('returns NaN and throws error when both arguments are not numbers', () => {
+        expect(add('6', '4')).toBeNaN();
+        expect(add('6', '4')).toThrow(Error('Invalid argument "6"'));
+    });
+    
+    test('returns NaN and throws error when one of the arguments is null', () => {
+        expect(add(2, null)).toBeNaN();
+        expect(add(2, null)).toThrow(Error('Invalid argument "null"'));
+    }   );
+    test('returns NaN and throws error when both arguments are null', () => {
+        expect(add(null, null)).toBeNaN();
+        expect(add(null, null)).toThrow(Error('Invalid argument "null"'));
+    });
+
+    test('returns NaN and throws error when one of the arguments is Infinity or -Infinity', () => {
+        expect(add(2, Infinity)).toBeNaN();
+        expect(add(2, -Infinity)).toBeNaN();
+        expect(add(2, Infinity)).toThrow(Error('Invalid argument "Infinity"'));
+    });
+
+    test('returns NaN when both arguments are Infinity or -Infinity', () => {
+        expect(add(Infinity, Infinity)).toBeNaN();
+        expect(add(-Infinity, -Infinity)).toBeNaN();
+        expect(add(Infinity, Infinity)).toThrow(Error('Invalid argument "Infinity"'));
+    });
+
+    test('returns NaN when one of the arguments is Boolean', () => {
+        expect(add(2, true)).toBeNaN();
+        expect(add(2, false)).toBeNaN();
+        expect(add(2, true)).toThrow(Error('Invalid argument "true"'));
+    });
 });
 
 describe('toString', () => {
+    test('per example, returns a string representation of an array', () => {
+        expect(toString([1, 2, 3])).toBe('1,2,3');
+    });
     test('returns a string representation of a number', () => {
         expect(toString(42)).toBe('42');
     });
@@ -51,11 +104,33 @@ describe('toString', () => {
     });
 
     test('returns a string representation of an object', () => {
-        expect(toString({ a: 1, b: 2 })).toBe('[object Object]');
+        expect(toString({ a: 1, b: 2 })).toBe('{ a: 1, b: 2 }');
     });
+
+    test('returns an empty string for null', () => {
+        expect(toString(null)).toBe('');
+    });
+
+    test('returns an empty string for undefined', () => {
+        expect(toString(undefined)).toBe('');
+    });
+
+    test('returns empty string when no argument is passed', () => {
+        expect(toString()).toBe('');
+    });
+
+    test('returns numbers with correct sign', () => {
+        expect(toString(-3)).toBe('-3');
+        expect(toString(-0)).toBe('-0');
+    });
+
+    
 });
 
 describe('divide', () => {
+    test('per example, returns the quotient of two numbers', () => {
+        expect(divide(6, 4)).toBe(1.5);
+    });
     test('returns the quotient of two positive numbers', () => {
         expect(divide(6, 3)).toBe(2);
     });
@@ -68,12 +143,47 @@ describe('divide', () => {
         expect(divide(6, -3)).toBe(-2);
     });
 
-    test('throws an error when dividing by zero', () => {
-        expect(() => divide(6, 0)).toThrow();
+    test('returns correct value when only one argument is passed', () => {
+        expect(divide(6)).toBe(6);
     });
+
+    test('returns correct value when one of the arguments is undefined', () => {
+        expect(divide(6, undefined)).toBe(6);
+    });
+
+    test('returns 1 when both arguments are undefined', () => {
+        expect(divide(undefined, undefined)).toBe(1);
+    });
+
+    test('throws an error when dividing by zero', () => {
+        expect(() => divide(6, 0)).toThrow(Error('Division by zero'));
+    });
+
+    test('throws an error when arguments are not numbers', () => {
+        expect(() => divide('6', '3')).toThrow(Error('Invalid argument "6"'));
+    });
+
+    test('throws an error when one of the arguments is null', () => {
+        expect(() => divide(6, null)).toThrow(Error('Invalid argument "null"'));
+    });
+
+    test('throws an error when both arguments are null', () => {
+        expect(() => divide(null, null)).toThrow(Error('Invalid argument "null"'));
+    });
+
+
 });
 
 describe('filter', () => {
+
+    test('per example, returns active users', () => {
+        const users = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred', 'active': false }
+        ]
+        expect(filter(users, ({ active }) => active)).toEqual([{ 'user': 'barney', 'active': true }]);
+    });
+
     test('returns an array with the elements that pass the condition', () => {
         expect(filter([1, 2, 3, 4], (n) => n % 2 === 0)).toEqual([2, 4]);
     });
@@ -85,15 +195,39 @@ describe('filter', () => {
     test('returns the original array when all elements pass the condition', () => {
         expect(filter([2, 4, 6], (n) => n % 2 === 0)).toEqual([2, 4, 6]);
     });
+
+    test('returns an empty array when the input is an empty array', () => {
+        expect(filter([], (n) => n % 2 === 0)).toEqual([]);
+    });
+
+    test('returns an empty array when the input is an empty object', () => {
+        expect(filter({}, (n) => n % 2 === 0)).toEqual([]);
+    });
+
+    test('returns an empty array when the input is a string', () => {
+        expect(filter('hello', (n) => n % 2 === 0)).toEqual([]);
+    });
+
+    test('will throw if predicate is not a function', () => {
+        expect(() => filter([1, 2, 3], 'hello')).toThrow(Error('predicate is not a function'));
+    });
+
+    test('will throw if predicate is not defined', () => {
+        expect(() => filter([1, 2, 3])).toThrow(Error('predicate is not defined'));
+    });
+
 });
 
 describe('isArrayLikeObject', () => {
+    test('per example, returns true for an array', () => {
+        expect(isArrayLikeObject([1, 2, 3])).toBe(true);
+    });
     test('returns true for an array', () => {
         expect(isArrayLikeObject([1, 2, 3])).toBe(true);
     });
 
-    test('returns true for an object with a length property', () => {
-        expect(isArrayLikeObject({ length: 3 })).toBe(true);
+    test('returns false for an object with a length property if object is not an Array', () => {
+        expect(isArrayLikeObject({ length: 3 })).toBe(false);
     });
 
     test('returns false for an object without a length property', () => {
@@ -103,6 +237,24 @@ describe('isArrayLikeObject', () => {
     test('returns false for a string', () => {
         expect(isArrayLikeObject('hello')).toBe(false);
     });
+
+    test('returns false for a function', () => {
+        expect(isArrayLikeObject(Function)).toBe(false);
+    });
+
+    test('returns false for null', () => {
+        expect(isArrayLikeObject(null)).toBe(false);
+    });
+
+    test('returns false for undefined', () => {
+        expect(isArrayLikeObject(undefined)).toBe(false);
+    });
+
+    test('returns false for arguments object', () => {
+        expect(isArrayLikeObject(arguments)).toBe(false);
+    });
+
+
 });
 
 describe('ceil', () => {
@@ -147,10 +299,12 @@ describe('upperFirst', () => {
 
 describe('countBy', () => {
     test('counts number of occurrences based on condition', () => {
-        expect(countBy([1, 2, 3, 4], (n) => n % 2 === 0)).toEqual({ 'true': 2, 'false': 2 });
+        const users = [{'user':'barney', 'active': true},{'user':'betty', 'active': false},{'user':'fred', 'active': true}]
+        expect(countBy(users, value => value.active)).toEqual({ 'true': 2, 'false': 1 });
     });
 
     test('returns empty object when no elements pass condition', () => {
-        expect(countBy([1, 3, 5], (n) => n % 2 === 0)).toEqual({});
+        const users = [{'user':'barney', 'active': true},{'user':'betty', 'active': false},{'user':'fred', 'active': true}]
+        expect(countBy(users, value => value.child)).toEqual({});
     });
 });
