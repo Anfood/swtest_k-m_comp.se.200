cdc11ece122775edfc3cb3ecfda9ce5d5e0d3de1
Kaisa Silkelä <kaisa.silkela@sofiadigital.com>
2023-11-29 17:43:34 +0200

    Add jest config for limiting coverage to only cover files that we implemented tests for.

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/add.js', 'src/toString.js',
+                          'scr/divide.js', 'src/filter.js', 
+                          'src/isArrayLikeObject.js', 'src/ceil.js',
+                          'src/difference.js', 'src/toInteger.js',
+                          'src/upperFirst.js', 'src/countBy.js'],
+  };
